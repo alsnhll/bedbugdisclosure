dbff60f297dad8ffa7292bf73c5a3826b65a6bf3
alsnhll <34223923+alsnhll@users.noreply.github.com>
2018-11-21 14:44:46 -0500

    Add files via upload

--- code/plotodes.R
+++ code/plotodes.R
@@ -1,14 +1,14 @@
 library(deSolve)
 
 # IMPORTANT: UPDATE THE WORKING DIRECTORY BELOW
-setwd("UPDATE_PATH/bedbugdisclosure")
+setwd("~/Dropbox/Research/SESYNC/Code")
 
 source("code/functions.R")
 
 # 1. Set baseline prevalence (p), renter selectivity (s), and years of simulation
 p <- 0.05
-s <- 1
-nyears <- 2
+s <- 0.5
+nyears <- 10
 
 # 2. Set parameter values manually
 SetParametersManual <- function(){
@@ -29,7 +29,8 @@ beta <- GetBeta(preparam, p)
 # 4. Either solve for initial conditions "at equilibrium"...
 param <- c(preparam, beta, base.prev=p)
 init <- GetInit(param)
-y0 <- c(init, 0, 0, 0)
+y0 <- c(init, 0, 0, 0, 0)
+names(y0)[5:8] <- c("Sv2","C_treat","C_turnover","C_vacancy")
 
 # OR
 
@@ -46,12 +47,23 @@ t <- seq(from=0, to=365*nyears+1, by=1)
 out <- ode(y=y0, times = t, func=SetODEs, parms=pp)
 
 # 7. Plot the output
-N = preparam[6]
-plot(t/365, out[,2], type="l", col="black", ylim=c(0,1000), 
-     ylab="Number of units", xlab="Year")
-lines(t/365, out[,3], col="red")
-lines(t/365, out[,4], col="black", lty=3)
-lines(t/365, out[,5], col="red", lty=3)
-legend("right", legend=c("Sr", "Sv", "Ir", "Iv"), bty="n",
-       col=c("black", "black", "red", "red"), lty=c(1,3,1,3), cex=0.8)
 
+plot(t/365, out[,2], type="l", col="black", ylim=c(0,1000), 
+     ylab="Number of units", xlab="Year") #Sr
+lines(t/365, out[,4], col="black", lty=3) #Sv
+lines(t/365, out[,6], col="black", lty=2) #Sv'
+lines(t/365, out[,3], col="red") #Ir
+lines(t/365, out[,5], col="red", lty=3) #Iv
+legend("right", legend=c("Sr", "Sv","Sv'", "Ir", "Iv"), bty="n",
+       col=c("black", "black","black", "red", "red"), lty=c(1,3,2,1,3),lwd=c(1,1,1,1,1), cex=0.8)
+
+Itot <- out[,3]+out[,5]
+Vtot <- out[,4]+out[,5]+out[,6]
+
+plot(t/365, Itot, type="l", col="red", lty=1, lwd=3, ylim=c(0,1000), 
+     ylab="Number of units", xlab="Year") #Iv+Ir
+lines(t/365, Vtot, col="blue", lty=3, lwd=3) #Sv+Sv'+Iv
+legend("right", legend=c("Total I","Total v"), bty="n",
+       col=c("red","blue"), lty=c(1,3),lwd=c(3,3), cex=0.8)
+
+print(out[dim(out)[1],])
